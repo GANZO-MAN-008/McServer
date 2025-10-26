532996e66b15b904512fc83d4df67d54af9446b0
GANZO-MAN-008 <154022325+GANZO-MAN-008@users.noreply.github.com>
2025-10-26 16:56:12 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ java -Xmx10G -Xms10G -jar /McServer/forge.jar nogui
 
 git add .
 git commit -m "initial commit"
---mount=type=secret,id=API_KEY \
-    git push https://$(cat /run/secrets/API_KEY)@github.com/GANZO-MAN-008/McServer.git master
+
+git push https://$(cat /run/secrets/API_KEY)@github.com/GANZO-MAN-008/McServer.git master
