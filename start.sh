bb1b9481c87ea04fe20ed128c1a0eb4d15ae5204
GANZO-MAN-008 <154022325+GANZO-MAN-008@users.noreply.github.com>
2025-10-26 17:46:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,8 +15,13 @@ git config --global user.email $EMAIL
 git config --global user.name $USERNAME
 
 # After server stops, try to push changes (if API_KEY exists)
+git config pull.rebase false
+git pull origin master
+
+
 git add .
 git commit -m "Server data update $(date)" || echo "Nothing to commit"
     
 API_KEY=$(echo $API_KEY)
-git push https://${API_KEY}@github.com/GANZO-MAN-008/McServer.git HEAD:master || echo "Push failed"
+git remote set-url origin https://${API_KEY}@github.com/GANZO-MAN-008/McServer.git
+git push origin master
